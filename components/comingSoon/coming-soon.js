e1fc903ed723243b7321bd3f6278ff266cfcfcc0
SalmanAhmed24 <salman.ahmed.abbasi.24@gmail.com>
2023-06-03 18:18:30 +0500

    new commit

---
+++ components/comingSoon/coming-soon.js
@@ -0,0 +1,14 @@
+import "./coming-soon.scss";
+function ComingSoon() {
+  return (
+    <section className="coming-soon-wrap">
+      <h1 className="main-h">Coming Soon!</h1>
+      <p className="coming-p">
+        Please be patient our site is under development and will be available
+        soon.
+      </p>
+    </section>
+  );
+}
+
+export default ComingSoon;
