f343b03c23ecc60cbf82c0ba4e0e13a3199732ae
SalmanAhmed24 <salman.ahmed.abbasi.24@gmail.com>
2023-06-07 08:17:05 +0500

    worked on the text issue

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,6 +6,16 @@ import "../styles/global.scss";
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link
+        rel="preconnect"
+        href="https://fonts.gstatic.com"
+        crossOrigin="true"
+      />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600;700;800;900&display=swap"
+        rel="stylesheet"
+      />
       <body>
         <TopNavbar />
         {children}
